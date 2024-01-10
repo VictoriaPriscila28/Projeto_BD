b58def2c30c888d1a0bbd3e4994e4ce90cf8f1dc
VictoriaPriscila28 <victoria.priscila.pl@gmail.com>
2024-01-09 23:12:42 -0300

    removed html code & add script.sql

---
+++ script.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE charnelle;
+/*CREATE TABLE itens (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    senha VARCHAR(100) NOT NULL
+);*/
+
+#select * from itens;
